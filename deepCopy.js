7c28f097901cd8d416b25eb797f1846dc96b3c13
Anton Ivanov <a.ivanov@cashdrive.ru>
2021-05-17 22:19:03 +0400

    feat: add deepCopy

---
+++ deepCopy.js
@@ -0,0 +1,26 @@
+const isObject = obj => typeof obj === 'object'
+
+const deepCopy = (object) => {
+
+    if(Array.isArray(object)) {
+        const copy = [...object]
+
+        copy.forEach((item, i) => {
+            if(isObject(item)) copy[i] = deepCopy(item)
+        })
+
+        return copy
+    }
+
+    if(isObject(object)) {
+        const copy = {...object}
+
+        for(let key in copy) copy[key] = deepCopy(copy[key])
+
+        return copy
+    }
+
+    return object
+}
+
+export default deepCopy;
